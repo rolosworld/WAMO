4bc6fdae72a273906d5bd19a7b890db8ab2b3d29
Rolando Gonzalez-Chevere <rolosworld@gmail.com>
2016-03-03 23:46:34 -0500

    - Initial SFV commit

---
+++ SFV/dropdown.js
@@ -0,0 +1,18 @@
+var frame_data = {
+    Birdie: 'birdie.csv',
+    Cammy: 'cammy.csv',
+    ChunLi: 'chunli.csv',
+    Dhalsim: 'dhalsim.csv',
+    FANG: 'fang.csv',
+    Karin: 'karin.csv',
+    Ken: 'ken.csv',
+    Laura: 'laura.csv',
+    MBison: 'mbison.csv',
+    Nash: 'nash.csv',
+    Necalli: 'necalli.csv',
+    Rashid: 'rashid.csv',
+    RMika: 'rmika.csv',
+    Ryu: 'ryu.csv',
+    Vega: 'vega.csv',
+    Zangief: 'zangief.csv'
+};
